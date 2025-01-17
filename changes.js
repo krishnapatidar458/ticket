818bc85d1dff1040ffd02c84503b88891d65f835
Krishna Patidar <92305156+krishnapatidar458@users.noreply.github.com>
2025-01-18 02:21:23 +0530

    Update changes.js

--- changes.js
+++ changes.js
@@ -27,7 +27,7 @@ let train = "Express";
 let train_hindi = "एक्सप्रेस";
 let date = "18/01/2025";
 let time = "02:10";
-let number = "623825591";
+let number = "623825571";
 let distance = "133";
 
 
