84fb3034d037c9cdf7e36f8e9dbbff5654ef6c92
Krishna Patidar <92305156+krishnapatidar458@users.noreply.github.com>
2025-10-05 17:23:52 +0530

    Update changes.js

--- changes.js
+++ changes.js
@@ -15,20 +15,20 @@
 
 
 
-let fare = 200;
-let source = "INDORE JS";
+let fare = 90;
+let source = "MALHARGARH";
 let source_hindi = "";
-let destination = "BHOPAL JS";
+let destination = "INDORE JS";
 let destination_hindi = "";
 let via = "";
-let adult = 2;
+let adult = 1;
 let child = 0;
 let train = "EXPRESS";
 let train_hindi = "एक्सप्रेस";
-let date = "03/10/2025";
-let time = "22:37";
+let date = "05/10/2025";
+let time = "5:37";
 let number = "6238255718";
-let distance = "262";
+let distance = "230";
 
 
 // show.html
